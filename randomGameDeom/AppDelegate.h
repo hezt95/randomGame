5fa59edf3006c7159e84cef6cfc5c98bfb95aee5
HeZitong <hezitong@HeZitongdeMacBook-Pro.local>
2014-12-05 22:45:52 +0800

    Initial Commit

---
+++ randomGameDeom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  randomGameDeom
+//
+//  Created by HeZitong on 14/12/5.
+//  Copyright (c) 2014年 HeZitong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ randomGameDeom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  randomGameDeom
+//
+//  Created by HeZitong on 14/12/5.
+//  Copyright (c) 2014年 HeZitong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
